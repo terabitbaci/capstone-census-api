14a6bb12274f2164395bbc0a1f5e7e1db77de7b7
terabitbaci <teresabacigalupi@gmail.com>
2017-09-14 11:29:37 -0700

    initial commit

---
+++ js/states/ak.js
@@ -0,0 +1,32 @@
+function populate(form) {
+    form.options.length = 0;
+    form.options[1] = new Option("Aleutians East Borough", "Aleutians East Borough");
+    form.options[2] = new Option("Aleutians West Census Area", "Aleutians West Census Area");
+    form.options[3] = new Option("Anchorage Municipality", "Anchorage Municipality");
+    form.options[4] = new Option("Bethel Census Area", "Bethel Census Area");
+    form.options[5] = new Option("Bristol Bay Borough", "Bristol Bay Borough");
+    form.options[6] = new Option("Denali Borough", "Denali Borough");
+    form.options[7] = new Option("Dillingham Census Area", "Dillingham Census Area");
+    form.options[8] = new Option("Fairbanks North Star Borough", "Fairbanks North Star Borough");
+    form.options[9] = new Option("Haines Borough", "Haines Borough");
+    form.options[10] = new Option("Hoonah-Angoon Census Area", "Hoonah-Angoon Census Area");
+    form.options[11] = new Option("Juneau City and Borough", "Juneau City and Borough");
+    form.options[12] = new Option("Kenai Peninsula Borough", "Kenai Peninsula Borough");
+    form.options[13] = new Option("Ketchikan Gateway Borough", "Ketchikan Gateway Borough");
+    form.options[14] = new Option("Kodiak Island Borough", "Kodiak Island Borough");
+    form.options[15] = new Option("Lake and Peninsula Borough", "Lake and Peninsula Borough");
+    form.options[16] = new Option("Matanuska - Susitna Borough", "Matanuska - Susitna Borough");
+    form.options[17] = new Option("Nome Census Area", "Nome Census Area");
+    form.options[18] = new Option("North Slope Borough", "North Slope Borough");
+    form.options[19] = new Option("Northwest Arctic Borough", "Northwest Arctic Borough");
+    form.options[20] = new Option("Petersburg Census Area", "Petersburg Census Area");
+    form.options[21] = new Option("Prince of Wales - Hyder Census Area", "Prince of Wales - Hyder Census Area");
+    form.options[22] = new Option("Sitka City and Borough", "Sitka City and Borough");
+    form.options[23] = new Option("Skagway Municipality", "Skagway Municipality");
+    form.options[24] = new Option("Southeast Fairbanks Census Area", "Southeast Fairbanks Census Area");
+    form.options[25] = new Option("Valdez - Cordova Census Area", "Valdez - Cordova Census Area");
+    form.options[26] = new Option("Wade Hampton Census Area", "Wade Hampton Census Area");
+    form.options[27] = new Option("Wrangell City and Borough", "Wrangell City and Borough");
+    form.options[28] = new Option("Yakutat City and Borough", "Yakutat City and Borough");
+    form.options[29] = new Option("Yukon - Koyukuk Census Area", "Yukon - Koyukuk Census Area");
+}
---
+++ js/states/dc.js
@@ -0,0 +1,3 @@
+function populate(form) {
+form.options.length = 0;
+form.options[1] = new Option("District of Columbia", "District of Columbia");
