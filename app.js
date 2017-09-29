0bc81b3365aa43937602125a1d13f692b7799d06
terabitbaci <teresabacigalupi@gmail.com>
2017-09-29 09:56:17 -0700

    hide search-results before click, and after new state/county in drop down

--- app.js
+++ app.js
@@ -1,10 +1,75 @@
 pic1 = new Image(16, 16);
 pic1.src = "style/loader.gif";
 
-$('.legend').hide();
+//STEP 1 - define global funcitons variable and objects
+
+function displaySearchResults(result, countyName, stateName) {
+
+    //create an empty variable to store one LI for each one the results
+    var buildTheHtmlOutput = "";
 
 
-//        $("#search-form").submit(function(event) {
+    $.each(result, function (resultKey, resultValue) {
+        //ignore the table head (the first row of the results)
+        if (resultKey > 0) {
+            var displayInValue = 0;
+            if (resultValue[0] !== null) {
+                displayInValue = resultValue[0];
+            }
+            var displayOutValue = "no data";
+            if (resultValue[1] !== null) {
+                displayOutValue = resultValue[1];
+            }
+
+
+            buildTheHtmlOutput += "<li class='column'>";
+            buildTheHtmlOutput += "<h4>with " + resultValue[4] + "</h4>";
+
+            //                buildTheHtmlOutput += "<h5>legend:</h5>";
+            //                buildTheHtmlOutput += "<h5>people moved in<i class='fa fa-long-arrow-right fa-2x' aria-hidden='true'></i>"
+
+
+
+            buildTheHtmlOutput += "<p>";
+
+            buildTheHtmlOutput += displayInValue + "&nbsp";
+            buildTheHtmlOutput += "<a href='#' title='Moved in'><i class='fa fa-long-arrow-right fa-2x' aria-hidden='true'></i></a>" + "&nbsp &nbsp";
+            buildTheHtmlOutput += "<a href='#' title='" + resultValue[3] + "'><i class='fa fa-map-marker fa-lg' aria-hidden='true' ></i></a>" + "&nbsp &nbsp";
+            buildTheHtmlOutput += "<a href='#' title='Moved out'><i class='fa fa-long-arrow-right fa-2x' aria-hidden='true' ></i></a>" + "&nbsp";
+            buildTheHtmlOutput += displayOutValue;
+
+            buildTheHtmlOutput += "</p>";
+
+            displayOutValue = resultValue[1];
+
+            buildTheHtmlOutput += "<p>" + "Net: " + (displayInValue - displayOutValue) + "</p>";
+            buildTheHtmlOutput += "</li>";
+        }
+    });
+
+    $("#search-results h4").html("Migrations for " + countyName + ", " + stateName);
+
+    //use the HTML output to show it in the index.html
+    $("#search-results ul").html('');
+    $("#search-results ul").html(buildTheHtmlOutput);
+    buildTheHtmlOutput = "";
+}
+
+
+
+//STEP 2 - use global funcitons variable and objects (triggers)
+
+//when the page loads
+$(document).ready(function () {
+    $('.legend').hide();
+    $('.results-wrapper').hide();
+});
+
+$(document).change("#county", function (event) {
+    $('.results-wrapper').hide();
+});
+
+//when search button is clicked
 $(document).on("click", ".search", function (event) {
 
 
@@ -12,6 +77,7 @@ $(document).on("click", ".search", function (event) {
     event.preventDefault();
 
     $('.legend').show();
+    $('.results-wrapper').show();
 
 
     //get the state value from the input box
@@ -85,56 +151,5 @@ $(document).on("click", ".search", function (event) {
             });
     }
 
-    function displaySearchResults(result, countyName, stateName) {
-
-        //create an empty variable to store one LI for each one the results
-        var buildTheHtmlOutput = "";
-
-
-        $.each(result, function (resultKey, resultValue) {
-            //ignore the table head (the first row of the results)
-            if (resultKey > 0) {
-                var displayInValue = 0;
-                if (resultValue[0] !== null) {
-                    displayInValue = resultValue[0];
-                }
-                var displayOutValue = 0;
-                if (resultValue[1] !== null) {
-                    displayOutValue = resultValue[1];
-                } else {
-                    displayOutValue = "no data";
-                }
-
-
-                buildTheHtmlOutput += "<li class='column'>";
-                buildTheHtmlOutput += "<h4>with " + resultValue[4] + "</h4>";
-
-                //                buildTheHtmlOutput += "<h5>legend:</h5>";
-                //                buildTheHtmlOutput += "<h5>people moved in<i class='fa fa-long-arrow-right fa-2x' aria-hidden='true'></i>"
-
-
-
-                buildTheHtmlOutput += "<p>";
-
-                buildTheHtmlOutput += displayInValue + "&nbsp";
-                buildTheHtmlOutput += "<a href='#' title='Moved in'><i class='fa fa-long-arrow-right fa-2x' aria-hidden='true'></i></a>" + "&nbsp &nbsp";
-                buildTheHtmlOutput += "<a href='#' title='" + resultValue[3] + "'><i class='fa fa-map-marker fa-lg' aria-hidden='true' ></i></a>" + "&nbsp &nbsp";
-                buildTheHtmlOutput += "<a href='#' title='Moved out'><i class='fa fa-long-arrow-right fa-2x' aria-hidden='true' ></i></a>" + "&nbsp";
-                buildTheHtmlOutput += displayOutValue;
-
-                buildTheHtmlOutput += "</p>";
-
-                displayOutValue = resultValue[1];
-
-                buildTheHtmlOutput += "<p>" + "Net: " + (displayInValue - displayOutValue) + "</p>";
-                buildTheHtmlOutput += "</li>";
-            }
-        });
-
-        $("#search-results h4").html("Migrations for " + countyName + ", " + stateName);
-
-        //use the HTML output to show it in the index.html
-        $("#search-results ul").html(buildTheHtmlOutput);
-    }
 
 });
